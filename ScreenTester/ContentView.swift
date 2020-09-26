56d300eb520eee32c2ae5ccf4d1542a4bce7bc50
Chris Eadie <chris.eadie@outlook.com>
2020-09-26 10:20:18 +0100

    Initial Commit

---
+++ ScreenTester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScreenTester
+//
+//  Created by Chris Eadie on 26/09/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScreenTester/ScreenTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenTesterApp.swift
+//  ScreenTester
+//
+//  Created by Chris Eadie on 26/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct ScreenTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
