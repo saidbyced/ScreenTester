acaa8f559b1eb3673448ddd39d0abf96743b06ab
Chris Eadie <chris.eadie@outlook.com>
2020-09-26 14:45:28 +0100

    Switch way list of colours and views is generated, extract colour cycling to function and add action sheet to select colour directly

--- ScreenTester/ContentView.swift
+++ ScreenTester/ContentView.swift
@@ -9,36 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var colorNumber = 0
-    private var colorList = [
-        Color.black,
-        Color.white,
-        // Red
-        Color(.sRGB, red: 1, green: 0, blue: 0, opacity: 1),
-        // Green
-        Color(.sRGB, red: 0, green: 1, blue: 0, opacity: 1),
-        // Blue
-        Color(.sRGB, red: 0, green: 0, blue: 1, opacity: 1),
-        // Yellow
-        Color(.sRGB, red: 1, green: 1, blue: 0, opacity: 1),
-        // Magenta
-        Color(.sRGB, red: 1, green: 0, blue: 1, opacity: 1),
-        // Cyan
-        Color(.sRGB, red: 0, green: 1, blue: 1, opacity: 1)
-    ]
+    @State private var showingColorMenu = false
+    private var colorList = TestColor.Option.allCases
+    private var actionSheetButtons: [ActionSheet.Button] {
+        var buttons = [ActionSheet.Button]()
+        for color in colorList {
+            let colorName = color.rawValue.capitalized
+            guard let colorNumber = colorList.firstIndex(of: color) else { fatalError() }
+            buttons.append(.default(Text(colorName)) { self.colorNumber = colorNumber })
+        }
+        return buttons
+    }
     
     var body: some View {
-        colorList[colorNumber]
+        TestColor(colorList[colorNumber]).view
             .ignoresSafeArea()
             .onTapGesture(count: 1, perform: {
-                if colorNumber >= colorList.count - 1 {
-                    colorNumber = 0
-                } else {
-                    colorNumber += 1
-                }
+                nextColor()
             })
             .onLongPressGesture {
-                colorNumber = 0
+                showingColorMenu.toggle()
             }
+            .actionSheet(isPresented: $showingColorMenu, content: {
+                ActionSheet(
+                    title: Text("Colour options"),
+                    message: Text("Choose colour to use for testing the screen"),
+                    buttons: actionSheetButtons
+                )
+            })
+    }
+    
+    func nextColor() {
+        if colorNumber >= colorList.count - 1 {
+            colorNumber = 0
+        } else {
+            colorNumber += 1
+        }
     }
 }
 
