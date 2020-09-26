1eef4151524e9b249fba95f86f6bce0e53ca7622
Chris Eadie <chris.eadie@outlook.com>
2020-09-26 11:14:51 +0100

    Create basic cycling colour view

--- ScreenTester/ContentView.swift
+++ ScreenTester/ContentView.swift
@@ -8,9 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var colorNumber = 0
+    private var colorList = [
+        Color.black,
+        Color.white,
+        // Red
+        Color(.sRGB, red: 1, green: 0, blue: 0, opacity: 1),
+        // Green
+        Color(.sRGB, red: 0, green: 1, blue: 0, opacity: 1),
+        // Blue
+        Color(.sRGB, red: 0, green: 0, blue: 1, opacity: 1),
+        // Yellow
+        Color(.sRGB, red: 1, green: 1, blue: 0, opacity: 1),
+        // Magenta
+        Color(.sRGB, red: 1, green: 0, blue: 1, opacity: 1),
+        // Cyan
+        Color(.sRGB, red: 0, green: 1, blue: 1, opacity: 1)
+    ]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        colorList[colorNumber]
+            .ignoresSafeArea()
+            .onTapGesture(count: 1, perform: {
+                if colorNumber >= colorList.count - 1 {
+                    colorNumber = 0
+                } else {
+                    colorNumber += 1
+                }
+            })
+            .onLongPressGesture {
+                colorNumber = 0
+            }
     }
 }
 
